8f2e13305c53288b1d975141316d3e71337d1de5
Abraham Rabago <abraham.rabago@envato.com>
2021-04-06 15:03:06 -0500

    Upload original files

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
-let app = new PIXI.Application({width: window.innerWidth, height: window.innerHeight});
-document.body.appendChild(app.view);
+window.onload = function() {
+    let app = new PIXI.Application({width: window.innerWidth, height: window.innerHeight});
+    document.body.appendChild(app.view);
 
-app.renderer.backgroundColor = 0x061639
-app.renderer.autoResize = true
+    app.renderer.backgroundColor = 0x061639
+    app.renderer.autoResize = true
+
+    var newStyle = document.createElement("style");
+    var style = "* {padding: 0; margin: 0}";
+    newStyle.appendChild(document.createTextNode(style));
+    document.head.appendChild(newStyle);
+
+    window.addEventListener("resize", function(event){ 
+        app.renderer.resize(window.innerWidth, window.innerHeight);
+    });
+}
